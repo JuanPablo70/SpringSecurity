bb67486a27a446c24b78a81cc8e24bfe6e6aac59
Juan Pablo <juanpasan2000@gmail.com>
2024-07-14 12:21:48 -0500

    User credentials in memory

--- src/main/java/com/springsecurity/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/springsecurity/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,6 +31,21 @@ public class BasicAuthSecurityConfiguration {
         http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("spring")
+                .password("{noop}security")
+                .roles(Roles.USER.toString())
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noop}security")
+                .roles(Roles.ADMIN.toString())
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
---
+++ src/main/java/com/springsecurity/learn_spring_security/basic/Roles.java
@@ -0,0 +1,6 @@
+package com.springsecurity.learn_spring_security.basic;
+
+public enum Roles {
+    USER,
+    ADMIN
+}
